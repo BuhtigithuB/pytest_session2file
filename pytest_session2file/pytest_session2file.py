afcde6767bfe6287d85f571457d518287d87efb3
Fabrice <fabrice.coulon74@gmail.com>
2017-04-24 16:51:09 +0200

    Make it work for Python 3

--- pytest_session2file/pytest_session2file.py
+++ pytest_session2file/pytest_session2file.py
@@ -50,7 +50,10 @@ def pytest_configure(config):
 
         def tee_write(s, **kwargs):
             oldwrite(s, **kwargs)
-            config._pytestsessionfile.write(s.encode('utf8'))
+            if (sys.version_info > (3, 0)):
+                config._pytestsessionfile.write(s)
+            else:
+                config._pytestsessionfile.write(s.encode('utf8'))
         tr._tw.write = tee_write
 
 
